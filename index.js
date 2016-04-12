68533d55ee45937060471fc73bf48be853eca143
Noah <nowarkdev@gmail.com>
2016-04-12 17:39:25 +0100

    @alexnaish refactor runners into local / browserstack

--- index.js
+++ index.js
@@ -8,20 +8,32 @@ var pipeHelper = require('./helpers/stdin');
 var testHelper = require('./helpers/fetchTests');
 
 program
-  .version(packageJson.version)
-  .option('-b, --browser <option>', 'Define test browser (defaults to "phantomjs")', 'phantomjs')
-  .option('-s, --suite <suite>', 'Define file to run (optional)')
-  .option('-t, --type <key>', 'Define subset of tests to run (optional)', 'all')
-  .option('-c, --config <filePath>', 'Define config file location (defaults to "config/scholar.js")', 'config/scholar.js')
-  .option('-d, --directory <testDirectory>', 'Define test files directory (defaults to "process.cwd()/test/")', 'test')
-  .parse(process.argv);
+    .version(packageJson.version)
+    .option('-b, --browser <option>', 'Define test browser (defaults to "phantomjs")', 'phantomjs')
+    .option('--browserstack', 'Define whether to use browserstack')
+    .option('--verbose', 'Define selenium log level')
+    .option('-s, --suite <suite>', 'Define file to run (optional)')
+    .option('-t, --type <key>', 'Define subset of tests to run (optional)', 'all')
+    .option('-c, --config <filePath>', 'Define config file location (defaults to "config/scholar.js")', 'config/scholar.js')
+    .option('-d, --directory <testDirectory>', 'Define test files directory (defaults to "process.cwd()/test/")', 'test')
+    .parse(process.argv);
 
-pipeHelper(function(parsedData){
-  program.data = parsedData;
-  var options = pick(program, ['data', 'browser', 'type', 'config']);
+pipeHelper(function (parsedData) {
+    program.data = parsedData;
+    var options = pick(program, ['data', 'browser', 'config', 'browserstack', 'verbose']);
+    testHelper(program.directory, program.suite, program.type, function (results) {
 
-  testHelper(program.directory, program.suite, program.type, function(results){
-    options.scenarios = results;
-    require('./lib/runners')[program.browser](options)
-  });
+        options.scenarios = results;
+
+        var runner = 'local';
+        if (program.browserstack) {
+            runner = 'browserstack';
+        }
+
+        if (program.browser === 'phantom') options.browser = 'phantomjs';
+
+        console.log(`<<<<<< Runner: ${runner} >>>>>>`);
+
+        require('./lib/runners')[runner](options)
+    });
 });
