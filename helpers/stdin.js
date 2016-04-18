9f4ae483049473695955a3d4ae9f9d39875e7762
Noah <nowarkdev@gmail.com>
2016-04-18 16:27:24 +0100

    transform stdin into a stream

--- helpers/stdin.js
+++ helpers/stdin.js
@@ -1,16 +1,33 @@
-module.exports = function(callback) {
-  var data = '';
-  var self = process.stdin;
-  self.on('readable', function() {
-      var chunk = this.read();
-      if (chunk !== null) {
-         data += chunk;
-      } else {
-        callback();
-      }
-  });
-  self.on('end', function() {
-    var config = JSON.parse(data.trim());
-    callback(config)
-  });
+const Transform = require('stream').Transform;
+
+module.exports = function (callback) {
+    if (process.stdin.isTTY) {
+        return callback();
+    }
+
+    const rs = new Transform({
+        transform: function (chunk, encoding, next) {
+            this.data = this.data || '';
+            if (chunk !== null) {
+                this.data += chunk;
+            }
+            this.push(chunk);
+            next();
+        },
+        flush: function (done) {
+            try {
+                callback(JSON.parse(this.data.trim()));
+            } catch (e){
+                callback()
+            } finally {
+                done();
+            }
+        }
+    });
+
+    process.stdin.pipe(rs)
+        .on('error', function (error) {
+            console.error('Pipe handler errored', error);
+            callback();
+        });
 };
